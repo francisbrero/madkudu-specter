7c0e5c85d59eaa5756ffd24aa1be46b657318489
Francis <francis@madkudu.com>
2017-10-18 00:08:51 -0700

    working prototype

--- myapp.js
+++ myapp.js
@@ -7,9 +7,15 @@ InboxSDK.load(1, 'sdk_test12_4ff1a33e18').then(function(sdk){
       console.log('compose view going away, time to clean up');
     });
 
-    // automagically add my salesforce bcc
-    composeView.on('recipientsChanged', function(event) {
-      composeView.setBccRecipients(['emailtosalesforce@dafh7g1kahuydcglgiq101kghlzp534dmi3bpmhoi878j3d13.41-mohleao.na35.le.salesforce.com']);      
+    // automagically add my salesforce bcc to the current bcc addresses at the time of sending
+    composeView.on('presending', function(event) {
+      var bccContacts = composeView.getBccRecipients();
+      var bccEmails = [];
+      for (var i = 0; i < bccContacts.length; i++) {
+          bccEmails.push(bccContacts[i].emailAddress);
+      };
+      bccEmails.push('emailtosalesforce@dafh7g1kahuydcglgiq101kghlzp534dmi3bpmhoi878j3d13.41-mohleao.na35.le.salesforce.com');
+      composeView.setBccRecipients(bccEmails);      
     });
 
     // add a link to your awesome calendar
@@ -24,7 +30,7 @@ InboxSDK.load(1, 'sdk_test12_4ff1a33e18').then(function(sdk){
 
     // add a link to your awesome calendar
     composeView.addButton({
-      title: "Add Magic links!",
+      title: "Add MadKudu links!",
       iconUrl: 'http://www.madkudu.com/static/images/madkudu_square.svg',
       onClick: function(event) {
         // created an encoded version of the email of the recipient
@@ -42,17 +48,19 @@ InboxSDK.load(1, 'sdk_test12_4ff1a33e18').then(function(sdk){
         // add our suffix to all links in the email
         var emailContent = composeView.getHTMLContent();
         var newEmailContent = urlify(emailContent, suffix);
+        // event.composeView.setBodyText(newEmailContent);
         event.composeView.setBodyHTML(newEmailContent);
+        // event.composeView.send();
       },
       orderHint: 2,
     });
   });
 
-  // a function that parses text looking for URLs that contain madkudu.com and end  and adds a suffix
+  // a function that parses text looking for URLs that contain madkudu.com and adds a suffix
   function urlify(text, suffix) {
-    var urlRegex = /(\bhttps?:\/\/(\S+|\S?)madkudu\.com(\S+|\S?)\/)/gi;
+    var urlRegex = /(\bhttps?:\/\/(\S+|\S?)madkudu\.com(\S+|\S?)\/\")/gi;
     return text.replace(urlRegex, function(url) {
-        return url + suffix;
-    })
+        return url.substring(0, url.length-1) + suffix + '"';
+    });
   }
 });
