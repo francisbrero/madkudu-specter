32c00876de045cf2322dcd74356b3de082acfde6
Francis <francis@madkudu.com>
2017-11-21 13:37:13 -0800

    UI element from Mixpanel

--- myapp.js
+++ myapp.js
@@ -1,3 +1,7 @@
+var path;
+var api_key;
+var api_secret;
+
 InboxSDK.load(1, 'sdk_test12_4ff1a33e18').then(function(sdk){
 
   // have the app load whenever a new composer is created
@@ -8,15 +12,16 @@ InboxSDK.load(1, 'sdk_test12_4ff1a33e18').then(function(sdk){
     });
 
     // automagically add my salesforce bcc to the current bcc addresses at the time of sending
-    composeView.on('presending', function(event) {
+    function bccSalesforce(composeView) {
       var bccContacts = composeView.getBccRecipients();
       var bccEmails = [];
       for (var i = 0; i < bccContacts.length; i++) {
           bccEmails.push(bccContacts[i].emailAddress);
       };
       bccEmails.push('emailtosalesforce@dafh7g1kahuydcglgiq101kghlzp534dmi3bpmhoi878j3d13.41-mohleao.na35.le.salesforce.com');
-      composeView.setBccRecipients(bccEmails);      
-    });
+      composeView.setBccRecipients(bccEmails);
+      return
+    };
 
     // add a link to your awesome calendar
     composeView.addButton({
@@ -49,6 +54,7 @@ InboxSDK.load(1, 'sdk_test12_4ff1a33e18').then(function(sdk){
         var emailContent = composeView.getHTMLContent();
         var newEmailContent = urlify(emailContent, suffix);
         event.composeView.setBodyHTML(newEmailContent);
+        bccSalesforce(composeView);
         event.composeView.send();
       },
       orderHint: 2,
@@ -67,4 +73,69 @@ InboxSDK.load(1, 'sdk_test12_4ff1a33e18').then(function(sdk){
         return url.substring(0, url.length-1) + '/' + suffix + '"'; 
     });
   }
+
+  // ======================================================================
+  // Now let's start showing some behavioral information in the UI
+  sdk.Conversations.registerMessageViewHandler(function(messageView) {
+    
+    messageView.on('destroy', function(event) {
+      console.log('message view going away, time to clean up');
+    });
+
+    var threadView = messageView.getThreadView();
+
+    // generate the dynamic element to add to the side bar
+    const el = document.createElement("div");
+    var info = '';  
+
+    // Get all contacts in the thread including the sender if the person has answered
+    var contacts = messageView.getRecipients();
+    contacts.push(messageView.getSender());    
+
+    // for all contacts, get the information from Mixpanel
+    for (var i = 0; i < contacts.length; i++) {
+      var contact = contacts[i];
+      var email = contact.emailAddress;
+      if(email.indexOf('@madkudu.com') == -1 && email.indexOf('emailtosalesforce@') == -1){
+        var newInfo = 'for email: ' + email;
+        
+        // Mixpanel variables        
+        var where = 'properties["$email"] == "' + email + '"';
+        var expire = new Date('2018', '12', '24').getTime() / 1000 + 3600;
+        var sig = calcMD5("api_key=" + api_key + "expire=" + expire + "where=" + where + api_secret);
+        path = 'https://mixpanel.com/api/2.0/engage?api_key=' + api_key + "&expire=" + expire + "&where=" + where;
+        path = path + "&sig=" + sig;
+        var mixpanelData = getMixpanel();
+        var mixpanelInfo = ' we have no tracking :-( ';
+        // Check if we got results back from the API call
+        if(mixpanelData.results.length >= 1){
+          mixpanelInfo = ' their customer fit is: ' + JSON.stringify(mixpanelData.results[0].$properties.mk_customer_fit) +' and they were last seen on the website: ' + JSON.stringify(mixpanelData.results[0].$properties.$last_seen);  
+        }        
+        info = info + newInfo + mixpanelInfo;
+      }       
+    }
+
+    // Push the content to the HTML
+    el.innerHTML = info
+
+    // Now add the side bar content
+    threadView.addSidebarContentPanel({
+      title: 'I am watching you!',
+      iconUrl: chrome.runtime.getURL('static/madkudu_square_256.png'),
+      el
+    });
+  });
+
+
+  // HTTP request to mixpanel URL
+  function getMixpanel() {
+    var xhr = new XMLHttpRequest();
+    // Note that any URL fetched here must be matched by a permission in
+    // the manifest.json file!
+    var url = path;
+    xhr.open('GET', url, false);
+    xhr.send();
+    return JSON.parse(xhr.responseText);
+  };
+
 });
